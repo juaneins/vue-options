3abb8cd21e36de3ac65710672f2fa2695e5d6fe4
juaneins_uio <ballesterosjg@gmail.com>
2023-09-24 12:13:48 -0500

    teleport in vue

---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="toggle">Modal</button>
+    <teleport to="body">
+      <div v-show="show" class="modal">
+        <h1>Titutlo</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam fugiat esse ut quam nisi,
+          odit consectetur cumque commodi? Obcaecati reiciendis aperiam corporis ea excepturi optio
+          possimus nisi. Commodi, ratione nihil?
+        </p>
+        <button @click="toggle">Cerrar</button>
+      </div>
+    </teleport>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    toggle() {
+      this.show = !this.show
+    }
+  }
+}
+</script>
