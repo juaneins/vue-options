8ff7eaf63d11fdd39d98555925b81cae0191a975
juaneins_uio <ballesterosjg@gmail.com>
2023-10-15 18:16:30 -0500

    Template refs

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,33 +1,43 @@
 <template>
   <div>{{ fullName }}</div>
   <div>{{ username }}</div>
+  <button ref="btn">Click!</button>
 </template>
 
 <script>
-import { toRefs, computed, inject } from 'vue'
+import { ref, toRefs, computed, inject, watch } from 'vue';
 
 export default {
   props: {
     firstName: String,
-    lastName: String
+    lastName: String,
   },
   setup(props, { expose }) {
-    const { firstName, lastName } = toRefs(props)
+    const { firstName, lastName } = toRefs(props);
 
     const fullName = computed(() => {
-      return `${firstName.value} ${lastName.value}`
-    })
+      return `${firstName.value} ${lastName.value}`;
+    });
     // se inyecta para que pueda ser usado por otros componentes
-    const username = inject('username')
+    const username = inject('username');
 
     expose({
-      fullName
-    })
+      fullName,
+    });
+
+    const btn = ref(null);
+
+    console.log(btn.value);
+
+    watch(btn, (valor) => {
+      console.log('btn valor: ', valor);
+    });
 
     return {
       fullName,
-      username
-    }
-  }
-}
+      username,
+      btn,
+    };
+  },
+};
 </script>
